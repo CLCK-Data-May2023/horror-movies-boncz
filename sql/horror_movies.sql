928b9c91b74ccada7a79905fc339d6f863d8c3ef
Ailene Bonczkowski <a.boncz@gmail.com>
2023-10-08 23:03:24 -0400

    completed assignment

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,1 +1,6 @@
 -- Add your SQL here
+SELECT movies.id AS Movie_ID, movies.name AS Movie_Title, movies.imdb_rating AS Rating
+FROM movies
+WHERE movies.genre = 'horror' AND movies.year <= 1985
+ORDER BY movies.imdb_rating DESC
+LIMIT 3;
